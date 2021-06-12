f0c3e92c476444fcacc673bcd010a5152fe6fc8a
Sebastian <bacconsebastian@gmail.com>
2021-06-12 14:09:39 -0300

    Subir a GitHub

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Game from './components/Game';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ta-Te-Ti</h1>
+      <p>By Sebastian Baccon</p>
+      <Game/>
+    </div>
+  );
+}
+
+export default App;
